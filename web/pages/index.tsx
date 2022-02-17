f67e3b225055ba6bb8d2cd30b87ce0691e5697cd
Atharva Deosthale <atharva.deosthale17@gmail.com>
2022-02-17 15:35:55 +0530

    style: working section done

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Action from '../components/Action'
 import Hero from '../components/Hero'
+import Working from '../components/Working'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
       <Hero />
       <Action />
+      <Working />
     </div>
   )
 }
