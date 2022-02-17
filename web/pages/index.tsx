0bbe8e9360e0ddf72fb2b66470769ddd7dfcac69
Atharva Deosthale <atharva.deosthale17@gmail.com>
2022-02-17 16:54:33 +0530

    style: responsiveness fix

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,15 +6,17 @@ import Working from '../components/Working'
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-6xl">
-      <Head>
-        <title>React &amp; Next.js snippets</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Hero />
-      <Action />
-      <Working />
-      <Installation />
+    <div className="w-screen overflow-y-hidden">
+      <div className="mx-auto max-w-6xl">
+        <Head>
+          <title>React &amp; Next.js snippets</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Hero />
+        <Action />
+        <Working />
+        <Installation />
+      </div>
     </div>
   )
 }
