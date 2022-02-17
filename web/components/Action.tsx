0be785255a3d09d3e6ead813465a3756906d082e
Atharva Deosthale <atharva.deosthale17@gmail.com>
2022-02-17 19:15:11 +0530

    fix: fixed gif

--- web/components/Action.tsx
+++ web/components/Action.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import React from 'react'
-import actionGif from '../public/action.gif'
 
 export default function Action() {
   return (
@@ -10,10 +9,10 @@ export default function Action() {
       </div>
       <div className="relative mt-16 rounded-xl border-4 border-[#24D1DC]">
         {/* Blur behind image */}
-        <div className="absolute top-1/2 left-1/2 h-[25rem] w-[50rem] -translate-y-1/2 -translate-x-1/2 bg-[#2A468E] blur-[140px]"></div>
+        <div className="absolute top-1/2 left-1/2 -z-10 h-[25rem] w-[50rem] -translate-y-1/2 -translate-x-1/2 bg-[#2A468E] blur-[140px]"></div>
         {/* Image */}
         <div className="overflow-hidden rounded-xl">
-          <Image src={actionGif} />
+          <img src="action.gif" className="z-10" />
         </div>
       </div>
     </div>
