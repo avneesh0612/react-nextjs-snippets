03d6cb3fc26eb364a657f589611bcdc6c0edaa48
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-02-18 13:32:04 +0530

    chore: add eslint in next.js website

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <div className="absolute top-0 z-20 flex h-20 w-full items-center justify-between p-7 xl:p-0">
       <div className="flex items-center">
-        <Link href="/">
+        <Link passHref href="/">
           <Logo className="h-10 w-10" />
         </Link>
         <span className="ml-5 text-lg font-medium">
@@ -18,6 +18,7 @@ export default function Navbar() {
         href="https://github.com/avneesh0612/react-nextjs-snippets"
         target="_blank"
         className="cursor-pointer"
+        rel="noreferrer"
       >
         <GitHubIcon className="h-6 w-6" />
       </a>
